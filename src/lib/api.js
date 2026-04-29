10db28555d9e44e7184a5558805e3352ea87b094
beksultanusmonov <beksultonusmonov16@gmail.com>
2026-04-29 11:12:32 +0500

    Back

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000/api'
+const API_BASE_URL = 'https://railway.com/project/237f61c7-da8d-4808-a3e3-f179e30008d4/service/74f619bb-3a97-4f0a-af1e-64e40c72c66c?environmentId=2c7f2e83-9646-4210-8b51-3a92645eaf9c' || 'http://localhost:4000/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
