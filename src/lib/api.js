b7a02953ce1d62abc8845b8f80b0057f5baf736c
beksultanusmonov <beksultonusmonov16@gmail.com>
2026-06-07 11:33:58 +0500

    render

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://v-back-production.up.railway.app/api'
+const API_BASE_URL = 'https://v-back-nkqs.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
